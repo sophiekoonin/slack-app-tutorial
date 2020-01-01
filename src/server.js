5bbb132d19a0b7c684b915856df4b27f67c84384
Sophie Koonin <sophie@localghost.dev>
2020-01-01 18:39:48 +0000

    step 2

--- src/server.js
+++ src/server.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.send('Hello world!')
+})
+
+const port = 6060
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+  console.log('Press Ctrl+C to quit.')
+})
